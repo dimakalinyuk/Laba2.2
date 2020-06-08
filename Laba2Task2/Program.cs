70fecc870d9d36d160efc3ae9527dafdfa3bbb70
Діма Калинюк <dima.kalinyuk.2019@ukr.net>
2020-06-08 12:54:41 +0300

    Laba2.2

---
+++ Laba2Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Laba2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = -3, y = 0;
+            Console.WriteLine("\n*****************************************\n");
+            Console.WriteLine("\tX\t\t\tY\n");
+            Console.WriteLine("*****************************************\n");
+            while (x <= 3)
+            {
+                y = (2 + Math.Pow(x, 3)) / (x + Math.Sqrt(13 * Math.Abs(x)));
+                Console.WriteLine("\t" + x + "\t\t" + y + "\n");
+                x += 0.5;
+            }
+            Console.ReadLine();
+        }
+    }
+}
